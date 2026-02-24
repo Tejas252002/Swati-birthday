66477735fb27e7358e3e44b98167713b6a03de0c
Tejas252002 <123626102+Tejas252002@users.noreply.github.com>
2026-02-24 12:40:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const memories = document.querySelectorAll('.memory');
+const correctPassword = "swati";  // 👈 change this
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-}, { threshold: 0.3 });
-
-memories.forEach(memory => {
-    observer.observe(memory);
-});
+function checkPassword() {
+    const entered = document.getElementById("passwordInput").value;
+    
+    if (entered === correctPassword) {
+        document.getElementById("password-screen").style.display = "none";
+        document.getElementById("website-content").style.display = "block";
+    } else {
+        document.getElementById("error").innerText = "Wrong password 💔";
+    }
+}
