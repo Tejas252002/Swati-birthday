3d0fd856bca3e458b92005f88ccaa001f871395b
Tejas252002 <123626102+Tejas252002@users.noreply.github.com>
2026-02-24 12:48:57 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const correctPassword = "swati";  // 👈 change this
+const correctPassword = "swati";
 
 function checkPassword() {
-    const entered = document.getElementById("passwordInput").value;
-    
-    if (entered === correctPassword) {
-        document.getElementById("password-screen").style.display = "none";
-        document.getElementById("website-content").style.display = "block";
-    } else {
-        document.getElementById("error").innerText = "Wrong password 💔";
-    }
+  const entered = document.getElementById("passwordInput").value;
+
+  if (entered === correctPassword) {
+    document.getElementById("password-screen").style.display = "none";
+    document.getElementById("website-content").style.display = "block";
+  } 
+  else {
+    document.getElementById("error").innerText = "Wrong password 💔";
+  }
 }
+
+/* Fade-in animation */
+const memories = document.querySelectorAll(".memory");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.3 });
+
+memories.forEach(memory => observer.observe(memory));
